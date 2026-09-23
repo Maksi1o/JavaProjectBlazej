1a5f2c2783c2939d3b80ef7aef72a88cff2a0971
blaze <blazej.maksimowski@gmail.com>
2026-09-23 16:44:32 +0200

    PD-1: Scoreboard

---
+++ src/main/java/pd1/Player.java
@@ -0,0 +1,49 @@
+package pd1;
+
+public class Player {
+    private String name;
+    private int[] scores;
+
+    public Player(String name, int[] scores) {
+        this.name = name;
+        this.scores = scores;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalScore() {
+        int totalScore = 0;
+        for (int score : scores) {
+            totalScore += score;
+        }
+        return totalScore;
+    }
+
+    public double getAverageScore() {
+        double averageCalculation = (double) getTotalScore() / scores.length;
+        return averageCalculation;
+    }
+
+    public int getMinScore() {
+        int minScore = scores[0];
+        for (int score : scores) {
+            if (score < minScore) {
+                minScore = score;
+
+            }
+        }
+        return minScore;
+    }
+
+    public int getMaxScore() {
+        int maxScore = scores[0];
+        for (int score : scores) {
+            if (score > maxScore) {
+                maxScore = score;
+            }
+        }
+        return maxScore;
+    }
+}
